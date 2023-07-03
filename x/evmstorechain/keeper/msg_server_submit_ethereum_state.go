833710605afa6f98c454baf58e64c59dc6ebdacb
0xsuryansh <suryansh.dev@proton.me>
2023-07-03 13:35:06 +0530

    only allow validators to vote for ethereum state

--- x/evmstorechain/keeper/msg_server_submit_ethereum_state.go
+++ x/evmstorechain/keeper/msg_server_submit_ethereum_state.go
@@ -5,20 +5,44 @@ import (
 
 	"EVMStoreChain/x/evmstorechain/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 )
 
 func (k msgServer) SubmitEthereumState(goCtx context.Context, msg *types.MsgSubmitEthereumState) (*types.MsgSubmitEthereumStateResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+    ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	vote := types.Vote{
-		Validator:   msg.Creator,
-		Blocknumber: msg.Blocknumber,
-		State:       msg.State,
-	}
-	k.AppendVote(ctx, vote)
+    // Get the validator set
+    validators := k.StakingKeeper.GetAllValidators(ctx)
 
-	_ = ctx
+    // Convert the creator to a ValAddress
+    creator, err := sdk.ValAddressFromBech32(msg.Creator)
+    if err != nil {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, err.Error())
+    }
 
-	return &types.MsgSubmitEthereumStateResponse{}, nil
+    // Check if the message creator is a validator
+    isValidator := false
+    for _, validator := range validators {
+        if validator.GetOperator().Equals(creator) {
+            isValidator = true
+            break
+        }
+    }
+
+    // If the message creator is not a validator, return an error
+    if !isValidator {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "only validators can vote")
+    }
+
+    // Handling the message
+    vote := types.Vote{
+        Validator:   msg.Creator,
+        Blocknumber: msg.Blocknumber,
+        State:       msg.State,
+    }
+    k.AppendVote(ctx, vote)
+
+    return &types.MsgSubmitEthereumStateResponse{}, nil
 }
+
