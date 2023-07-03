bdb7c2052b3737e4cc24dd2c86262aa32726b588
0xsuryansh <suryansh.dev@proton.me>
2023-07-03 13:44:49 +0530

    only allow validators to vote for ethereum state

--- x/evmstorechain/keeper/msg_server_submit_ethereum_state.go
+++ x/evmstorechain/keeper/msg_server_submit_ethereum_state.go
@@ -12,9 +12,6 @@ import (
 func (k msgServer) SubmitEthereumState(goCtx context.Context, msg *types.MsgSubmitEthereumState) (*types.MsgSubmitEthereumStateResponse, error) {
     ctx := sdk.UnwrapSDKContext(goCtx)
 
-    // Get the validator set
-    validators := k.StakingKeeper.GetAllValidators(ctx)
-
     // Convert the creator to a ValAddress
     creator, err := sdk.ValAddressFromBech32(msg.Creator)
     if err != nil {
@@ -22,22 +19,16 @@ func (k msgServer) SubmitEthereumState(goCtx context.Context, msg *types.MsgSubm
     }
 
     // Check if the message creator is a validator
-    isValidator := false
-    for _, validator := range validators {
-        if validator.GetOperator().Equals(creator) {
-            isValidator = true
-            break
-        }
-    }
+    validator, found := k.StakingKeeper.GetValidator(ctx, creator)
 
     // If the message creator is not a validator, return an error
-    if !isValidator {
+    if !found {
         return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "only validators can vote")
     }
 
     // Handling the message
     vote := types.Vote{
-        Validator:   msg.Creator,
+        Validator:   validator.GetOperator().String(),
         Blocknumber: msg.Blocknumber,
         State:       msg.State,
     }
